0e51effd8a8ea9046ecc87a9cc66ece78d3f62c7
Jessica Aline <jess.alineoliveira@gmail.com>
2025-12-03 11:19:43 -0300

    adjusting for each logic in subnets

--- Connectivity/subnets.tf
+++ Connectivity/subnets.tf
@@ -1,13 +1,27 @@
 #Subnets DMZ e BD privadas
+locals {
+  private_subnets = {
+    dmz = {
+      name = "dmz-subnet-private"
+      index = 0
+    }
+    bd = {
+      name = "bd-subnet-private"
+      index = 1
+    }
+  }
+}
+
 resource "aws_subnet" "dzm_bd_private_subnet" {
-  count = 2
+ for_each = local.private_subnets
+
   vpc_id     = aws_vpc.dev.id
-  cidr_block = cidrsubnet(aws_vpc.dev.cidr_block, 8, count.index + 1) #primeira e segunda subnet da VPC
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block = cidrsubnet(aws_vpc.dev.cidr_block, 8, each.value.index + 1) #primeira e segunda subnet da VPC
+  availability_zone = data.aws_availability_zones.available.names[each.value.index]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = count.index == 0 ? "dmz_subnet_private" : "bd_subnet_private"
+    Name = each.value.name
     Tier = "private"
   }
 }
